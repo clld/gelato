7a0d7baea18c6d38b29b40fab306b62bf440fe40
chrzyki <christoph@functor-argument.net>
2018-01-12 11:29:20 +0100

    Update Gelato to CLLD 4.0.

--- gelato/tests/test_functional.py
+++ gelato/tests/test_functional.py
@@ -1,12 +1,21 @@
+from __future__ import print_function, unicode_literals
 from clldutils.path import Path
-from clld.tests.util import TestWithApp
+#from clld.tests.util import TestWithApp
 
 import gelato
 
+import pytest
 
-class Tests(TestWithApp):
-    __cfg__ = Path(gelato.__file__).parent.joinpath('..', 'development.ini').resolve()
-    __setup_db__ = False
 
-    def test_home(self):
-        res = self.app.get('/', status=200)
+@pytest.mark.parametrize(
+    "method,path",
+    [
+        ('get_html', '/'),
+    ])
+
+def test_pages(app, method, path):
+    getattr(app, method)(path)
+
+
+# def test_wals(app):
+# app.get('/wals/9', status=404)
