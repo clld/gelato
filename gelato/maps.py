97dc4449975b228867d0bcbc1c027fa3cdb025dc
xrotwang <xrotwang@googlemail.com>
2018-12-06 15:22:45 +0100

    load samples in separate layers per panel

--- gelato/maps.py
+++ gelato/maps.py
@@ -1,6 +1,11 @@
 from __future__ import unicode_literals
 
-from clld.web.maps import ParameterMap, Map, FilterLegend
+from sqlalchemy.orm import joinedload
+from clld.web.maps import ParameterMap, Map, FilterLegend, Layer
+from clld.web.adapters.geojson import GeoJsonLanguages
+from clld.db.meta import DBSession
+
+from gelato.models import Panel
 
 
 class MeasureMap(ParameterMap):
@@ -8,10 +13,24 @@ class MeasureMap(ParameterMap):
         return {'icon_size': 15}
 
 
+class PanelSamples(GeoJsonLanguages):
+    def feature_iterator(self, ctx, req):
+        return ctx.samples
+
+
 class SamplesMap(Map):
     def __init__(self, ctx, req, eid='map', col=None, dt=None):
         self.col, self.dt = col, dt
         Map.__init__(self, ctx, req, eid=eid)
+
+    def get_layers(self):
+        for panel in DBSession.query(Panel).options(joinedload(Panel.samples)):
+            yield Layer(
+                panel.id,
+                panel.name,
+                PanelSamples(None).render(panel, self.req, dump=False),
+            )
+
 
     def get_legends(self):
         for legend in super(SamplesMap, self).get_legends():
